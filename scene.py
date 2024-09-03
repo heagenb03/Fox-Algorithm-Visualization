e2a9186e7e50857a2f7abd9a6c971ac038e2ddfe
heagenb03 <heagenbell@outlook.com>
2024-09-03 18:01:57 -0500

    new file:   media/texts/0cfca9a797ddecbc.svg 	new file:   media/texts/1a137a37a9fcc121.svg 	new file:   media/texts/2329f603d8e910e0.svg 	new file:   media/texts/28aa894e24a57d7b.svg 	new file:   media/texts/341bb7c64ea0aeda.svg 	new file:   media/texts/3e02066cbe7ef168.svg 	new file:   media/texts/4a0d959aff38410e.svg 	new file:   media/texts/55e28f23661b97c9.svg 	new file:   media/texts/652d6cbd8c530460.svg 	new file:   media/texts/79b1525f093c1805.svg 	new file:   media/texts/7d41a1648d588c7c.svg 	new file:   media/texts/8a36712cd1c322cb.svg 	new file:   media/texts/8d183aa8d74f6047.svg 	new file:   media/texts/95c26e88e6940289.svg 	new file:   media/texts/a8bbef74b6c8d7fa.svg 	new file:   media/texts/b7bc0c1f569dcd29.svg 	new file:   media/texts/f3e22233ada50f26.svg 	new file:   media/texts/f667ce89a495ce7c.svg 	new file:   media/videos/scene/480p15/Fox.mp4 	new file:   media/videos/scene/480p15/partial_movie_files/Fox/1185818338_2981119699_223132457.mp4 	new file:   media/videos/scene/480p15/partial_movie_files/Fox/1185818338_3159247908_223132457.mp4 	new file:   media/videos/scene/480p15/partial_movie_files/Fox/1185818338_3799572805_223132457.mp4 	modified:   media/videos/scene/480p15/partial_movie_files/Fox/partial_movie_file_list.txt 	modified:   scene.py

--- scene.py
+++ scene.py
@@ -1,32 +1,58 @@
-from manim import *
+from manim import VGroup, Rectangle, Scene, Text, RED_E, WHITE, YELLOW, RIGHT, ORIGIN, FadeIn
 from constants import *
 
-def createBlankTextbox():
-    result = VGroup()
-    box = Rectangle(
-        height=1,
-        width=1,
-        fill_color=RED_E,
-        fill_opacity=0.8,
-        stroke_color=WHITE
-    )
+#Creates the individual "box" within a matrix
+def createMatrixBox():
+    matrixBox = Rectangle(
+            height=1,
+            width=1,
+            fill_color=RED_E,
+            fill_opacity=0.8,
+            stroke_color=WHITE
+        )
     
-    result.add(box)
+    return matrixBox
+
+#Intializes the "box" as empty
+def createBlankMatrixEntries():
+    result = VGroup() 
+    result.add(createMatrixBox())
     return result
 
+#Intailizes a "box" with entries
+def createMatrixEntries(number, textColor):
+    result = VGroup()
+    entry = Text(str(number), font_size=24, color=textColor)
+    result.add(createMatrixBox(), entry)
+    return result
+
+#Creates a Matrix with blank "boxes"
 def createBlankMatrix():
     matrix = VGroup()
     box_list = []
     for i in range(MATRIX_SIZE):
-        box_list.append(createBlankTextbox())
+        box_list.append(createBlankMatrixEntries())
     matrix.add(*box_list)
+    
     matrix.arrange_in_grid(rows=MATRIX_ROW_CT, cols=MATRIX_COL_CT, buff=0.25)
     return matrix
-        
+
+#Creates a Matrix with "boxes" with enteries
+def createMatrix(textColor):
+    matrix = VGroup()
+    box_list = []
+    for i in range(MATRIX_SIZE):
+        box_list.append(createMatrixEntries(i, textColor))
+    matrix.add(*box_list)
+    
+    matrix.arrange_in_grid(rows=MATRIX_ROW_CT, cols=MATRIX_COL_CT, buff=0.25)
+    return matrix
+
+
 class Fox(Scene):
     def construct(self):
-        matrixA = createBlankMatrix()
-        matrixB = createBlankMatrix()
+        matrixA = createMatrix(WHITE)
+        matrixB = createMatrix(YELLOW)
         matrixC = createBlankMatrix()
         
         matrices = VGroup(
