af624b48555185d764607c40d97f69e546db6690
heagenb03 <heagenbell@outlook.com>
2024-09-09 14:44:19 -0500

    modified:   __pycache__/constants.cpython-312.pyc 	modified:   __pycache__/scene.cpython-312.pyc 	modified:   constants.py 	new file:   media/Tex/383d1fb9b7d92e82.svg 	new file:   media/Tex/383d1fb9b7d92e82.tex 	new file:   media/Tex/9c5cd0ba4bb1a5c3.svg 	new file:   media/Tex/9c5cd0ba4bb1a5c3.tex 	modified:   media/videos/scene/480p15/Fox.mp4 	new file:   media/videos/scene/480p15/partial_movie_files/Fox/1185818338_2645949627_223132457.mp4 	new file:   media/videos/scene/480p15/partial_movie_files/Fox/1185818338_991587331_223132457.mp4 	new file:   media/videos/scene/480p15/partial_movie_files/Fox/624642324_1141282389_3004754247.mp4 	new file:   media/videos/scene/480p15/partial_movie_files/Fox/624642324_1141282389_3138785764.mp4 	new file:   media/videos/scene/480p15/partial_movie_files/Fox/624642324_1704852926_179803108.mp4 	new file:   media/videos/scene/480p15/partial_movie_files/Fox/624642324_2794769226_3431171932.mp4 	new file:   media/videos/scene/480p15/partial_movie_files/Fox/624642324_2815696610_1024777987.mp4 	new file:   media/videos/scene/480p15/partial_movie_files/Fox/624642324_3539873854_1835751371.mp4     new file:   media/videos/scene/480p15/partial_movie_files/Fox/624642324_3609235514_1024777987.mp4 	new file:   media/videos/scene/480p15/partial_movie_files/Fox/624642324_4080107171_1835751371.mp4 	new file:   media/videos/scene/480p15/partial_movie_files/Fox/624642324_4097672759_1024777987.mp4 	modified:   media/videos/scene/480p15/partial_movie_files/Fox/partial_movie_file_list.txt 	modified:   scene.py

--- scene.py
+++ scene.py
@@ -1,5 +1,8 @@
-from manim import VGroup, Rectangle, Scene, Text, RED_E, WHITE, YELLOW, RIGHT, ORIGIN, FadeIn
+from manim import VGroup, Rectangle, Scene, Text, FadeIn, FadeOut, MathTex, np, RED_E, WHITE, RIGHT, ORIGIN, UL, UR, DOWN, LEFT
 from constants import *
+
+
+#Intialize Basic Callback Functions
 
 #Creates the individual "box" within a matrix
 def createMatrixBox():
@@ -17,53 +20,137 @@ def createMatrixBox():
 def createBlankMatrixEntries():
     result = VGroup() 
     result.add(createMatrixBox())
+    
     return result
 
-#Intailizes a "box" with entries
-def createMatrixEntries(number, textColor):
+#Intailizes a "box" with one entry
+def createMatrixEntry(num, txt_color):
     result = VGroup()
-    entry = Text(str(number), font_size=24, color=textColor)
+    entry = Text(str(num), font_size=MATRIX_FONT_SIZE, color=txt_color)
+    
     result.add(createMatrixBox(), entry)
+    
     return result
 
-#Creates a Matrix with blank "boxes"
-def createBlankMatrix():
+#Intailize a "box" with two enteries
+def createTwoMatrixEntries(first_num, second_num, first_txt_color, second_txt_color):
+    result = VGroup()
+    box = createMatrixBox()
+    
+    first_entry = Text(str(first_num), font_size=MATRIX_FONT_SIZE, color=first_txt_color)
+    second_entry = Text(str(second_num), font_size=MATRIX_FONT_SIZE, color=second_txt_color)
+    first_entry.align_to(box, UL).shift(DOWN * 0.1 + RIGHT * 0.1)
+    second_entry.align_to(box, UR).shift(DOWN * 0.1 + LEFT * 0.1)
+    
+    result.add(box, first_entry, second_entry)
+    
+    return result
+    
+#Scene 1
+
+#Creates Matrix A with "boxes" with enteries
+def createMatrixAScene1():
     matrix = VGroup()
     box_list = []
-    for i in range(MATRIX_SIZE):
+    
+    for i in range(MATRIX_COL_CT*MATRIX_ROW_CT):
+        box_list.append(createMatrixEntry(MATRIX_A_NUMBERS[i], MATRIX_A_COLOR))
+    
+    matrix.add(*box_list)
+    matrix.arrange_in_grid(rows=MATRIX_ROW_CT, cols=MATRIX_COL_CT, buff=MATRIX_BUFFER)
+    
+    return matrix
+
+#Creates Matrix B with "boxes" with enteries
+def createMatrixBScene1():
+    matrix = VGroup()
+    box_list = []
+    
+    for i in range(MATRIX_COL_CT*MATRIX_ROW_CT):
+        box_list.append(createMatrixEntry(MATRIX_B_NUMBERS[i], MATRIX_B_COLOR))
+        
+    matrix.add(*box_list)
+    matrix.arrange_in_grid(rows=MATRIX_ROW_CT, cols=MATRIX_COL_CT, buff=MATRIX_BUFFER)
+    
+    return matrix
+
+#Creates a Matrix C with blank "boxes"
+def createMatrixCScene1():
+    matrix = VGroup()
+    box_list = []
+    
+    for i in range(MATRIX_COL_CT*MATRIX_ROW_CT):
         box_list.append(createBlankMatrixEntries())
     matrix.add(*box_list)
     
-    matrix.arrange_in_grid(rows=MATRIX_ROW_CT, cols=MATRIX_COL_CT, buff=0.25)
+    matrix.arrange_in_grid(rows=MATRIX_ROW_CT, cols=MATRIX_COL_CT, buff=MATRIX_BUFFER)
     return matrix
 
-#Creates a Matrix with "boxes" with enteries
-def createMatrix(textColor):
+#Scene 2
+
+#Create Matrix C with numbers from Matrix A & C
+def createMatrixCScene2():
     matrix = VGroup()
     box_list = []
-    for i in range(MATRIX_SIZE):
-        box_list.append(createMatrixEntries(i, textColor))
-    matrix.add(*box_list)
     
-    matrix.arrange_in_grid(rows=MATRIX_ROW_CT, cols=MATRIX_COL_CT, buff=0.25)
+    for i in range(MATRIX_COL_CT*MATRIX_ROW_CT):
+        box_list.append(createTwoMatrixEntries(MATRIX_A_NUMBERS[i], MATRIX_B_NUMBERS[i], MATRIX_A_COLOR, MATRIX_B_COLOR))
+        
+    matrix.add(*box_list)
+    matrix.arrange_in_grid(rows=MATRIX_ROW_CT, cols=MATRIX_COL_CT, buff=MATRIX_BUFFER)
+    
     return matrix
-
-
+    
+    
 class Fox(Scene):
     def construct(self):
-        matrixA = createMatrix(WHITE)
-        matrixB = createMatrix(YELLOW)
-        matrixC = createBlankMatrix()
+        #Scene 1 - Fade in matrices + signs & fade out matrixA, B, & signs
+        
+        #Intialize Matrices
+        matrixA_scene1 = createMatrixAScene1()
+        matrixB_scene1 = createMatrixBScene1()
+        matrixC_scene1 = createMatrixCScene1()
+        
+        #Intialize Signs
+        multi_sign = MathTex("\\times")
+        equal_sign = MathTex("=")
         
         matrices = VGroup(
-            matrixA,
-            matrixB,
-            matrixC
+            matrixA_scene1,
+            multi_sign,
+            matrixB_scene1,
+            equal_sign,
+            matrixC_scene1
         ).arrange(RIGHT, buff=0.25)
         
         matrices.move_to(ORIGIN)
         
+        self.play(FadeIn(matrices))
+        self.wait(1)
+        
+        animations = []
+        for i in range(MATRIX_ROW_CT*MATRIX_COL_CT):
+            numberes = matrixA_scene1[i][1]
+            box_center = matrixC_scene1[i].get_center()
+            offset = np.array([-0.32, 0.28, 0])
+            target_position = box_center + offset
+            animations.append(numberes.animate.move_to(target_position)) 
+            
+            numberes = matrixB_scene1[i][1]
+            box_center = matrixC_scene1[i].get_center()
+            offset = np.array([0.32, 0.28, 0])
+            target_position = box_center + offset
+            animations.append(numberes.animate.move_to(target_position)) 
+            
+        self.play(*animations)
+        self.wait(1)
+        
+        #Scene 2
+        matrixC_scene2 = createMatrixCScene2()
+        
         self.play(
-            FadeIn(matrices)
+            FadeOut(matrices),
+            FadeIn(matrixC_scene2.shift(RIGHT * 4.27))
         )
         
+        self.wait(1)
