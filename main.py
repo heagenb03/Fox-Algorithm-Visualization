4e4666f549b069bfef5b62c03f1893ad6bea2206
heagenb03 <heagenbell@outlook.com>
2024-11-14 11:57:06 -0600

    modified:   update animations per updated scene3 and new scene 4

--- main.py
+++ main.py
@@ -8,20 +8,22 @@ from scenes.intial import Intial
 from scenes.scene1 import Scene1
 from scenes.scene2 import Scene2
 from scenes.scene3 import Scene3
+from scenes.scene4 import Scene4
 
 class Fox(Scene):
     def construct(self):
+        scene1 = Scene1()
+        scene2 = Scene2()
+        scene3 = Scene3()
+        scene4 = Scene4()
+        
         """
         Scene 1 
             #1. Create Matrix A, B, C
             #2. Move Aij & Bij values to corresponding Cij values / Fadeout Matrix A & B
         """
         
-        #1
-        scene1 = Scene1()
-        scene2 = Scene2()
-        scene3 = Scene3()
-        
+        #1        
         multi_sign = MathTex("\\times")
         equal_sign = MathTex("=")
         
@@ -35,7 +37,7 @@ class Fox(Scene):
             matrixB_scene1,
             multi_sign,
             matrixA_scene1
-        ).arrange(LEFT, buff=MATRIX_BUFFER)
+        ).arrange(LEFT, buff=MATRIX_BUFFER*2)
         
         matrices.move_to(ORIGIN)
         
@@ -130,24 +132,51 @@ class Fox(Scene):
             self.wait(1)
             
             #3
-            move_animations, transform_animations, intial_fade_in_animations, final_fade_in_animations, intial_fade_out_animations, final_fade_out_animations = scene3.updateComputedCValues(matrixC_scene2)
+            transform_animations, intial_move_animations, final_move_animations, intial_fade_in_animations, final_fade_in_animations, intial_fade_out_animations, final_fade_out_animations = scene3.updateComputedCValues(matrixC_scene2)
             total_fade_out_animations.extend(final_fade_out_animations)
             
             self.play(*intial_fade_in_animations)
             self.wait(1)
-            self.play(*move_animations)
-            self.wait(0.5)
-            self.play(*intial_fade_out_animations,
-                    *final_fade_in_animations)
+            self.play(*intial_move_animations)
+            self.wait(0.15)
+            self.play(*final_fade_in_animations,
+                      *intial_fade_out_animations)
             self.wait(1)
-            self.play(*transform_animations)
-            self.wait(2)
-            self.play(*total_fade_out_animations, runtime=5)
+            self.play(*final_move_animations)
+            self.wait(0.25)
+            self.play(*transform_animations,
+                      *total_fade_out_animations)
             self.wait(2)
             
             shift_count += 1
         
         """
         Scene 4
-            1.
+            1. Display finished Matrix C
+            2. Return to intial state of Matrix A, B, C
         """
+
+        #1
+        fade_out_animations, move_animations = scene4.fadeOutEntries(matrixC_scene2), scene4.moveCEnteriesToCenter(matrixC_scene2)
+        self.play(*fade_out_animations,
+                *move_animations)
+        self.wait(2)
+        
+        #2
+        move_animations = scene4.moveMatrixCtoOriginalPosition(matrixC_scene1, matrixC_scene2)
+        self.play(*move_animations)
+        self.wait(1)
+        
+        matrixB_scene4 = scene1.createMatrixB()
+        matrixA_scene4 = scene1.createMatrixA()
+        
+        matrices = VGroup(
+            equal_sign,
+            matrixB_scene4,
+            multi_sign,
+            matrixA_scene4
+        ).arrange(LEFT, buff=MATRIX_BUFFER*2)
+        matrices.move_to(ORIGIN).shift(LEFT * 1.5)
+        
+        self.play(FadeIn(matrices))
+        self.wait(3)
